d323d3e8a881f0bc19fd7f5c937fb983b0ddf4d8
Jan Ludwina <30725330+Miosp@users.noreply.github.com>
2024-10-12 11:17:14 +0200

    Initial Commit

---
+++ src/main/kotlin/com/sofixit/besthacksbackend/BesthacksBackendApplication.kt
@@ -0,0 +1,11 @@
+package com.sofixit.besthacksbackend
+
+import org.springframework.boot.autoconfigure.SpringBootApplication
+import org.springframework.boot.runApplication
+
+@SpringBootApplication
+class BesthacksBackendApplication
+
+fun main(args: Array<String>) {
+  runApplication<BesthacksBackendApplication>(*args)
+}
---
+++ src/test/kotlin/com/sofixit/besthacksbackend/BesthacksBackendApplicationTests.kt
@@ -0,0 +1,13 @@
+package com.sofixit.besthacksbackend
+
+import org.junit.jupiter.api.Test
+import org.springframework.boot.test.context.SpringBootTest
+
+@SpringBootTest
+class BesthacksBackendApplicationTests {
+
+  @Test
+  fun contextLoads() {
+  }
+
+}
