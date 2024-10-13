0dde06e402e12bccfd98efc93eff927246e61111
Jan Ludwina <30725330+Miosp@users.noreply.github.com>
2024-10-13 06:36:23 +0200

    Templating +^27

--- src/main/kotlin/com/sofixit/besthacksbackend/functionality/templating/TemplateName.kt
+++ src/main/kotlin/com/sofixit/besthacksbackend/functionality/templating/TemplateName.kt
@@ -2,5 +2,5 @@ package com.sofixit.besthacksbackend.functionality.templating
 
 @Suppress("unused")
 enum class TemplateName(val value: String) {
-  RESUME1("resume1"), RESUME2("resume2"), RESUME3("resume3")
+  RESUME1("resume1"), RESUME2("resume2"), RESUME3("resume3"), RESUME4("resume4")
 }
